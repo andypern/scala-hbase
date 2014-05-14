635c7be68b975cc4c35616b9e2d481659fef2420
Andy Pernsteiner <andypern@gmail.com>
2014-05-14 17:17:27 -0400

    got rid of some unneeded classes

--- hbase_basics.scala
+++ hbase_basics.scala
@@ -9,26 +9,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.Delete;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Row;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.util.Bytes;
 
 
-//import org.apache.hadoop.hbase.HBaseConfiguration
-//import org.apache.hadoop.hbase.client.{HBaseAdmin,HTable,Put,Get}
-//import org.apache.hadoop.hbase.util.Bytes
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{HBaseAdmin,HTable,Put,Get}
+import org.apache.hadoop.hbase.util.Bytes
 
 object hbasics {
 
